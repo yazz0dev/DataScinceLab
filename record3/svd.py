a16fd2879f9eefbd999067f638cd95c8adddf79c
yaseen <yzac1569@gmail.com>
2025-07-15 10:39:31 +0530

    finish

--- record3/svd.py
+++ record3/svd.py
@@ -5,9 +5,7 @@ import numpy as np
 
 A = np.array([[1,2,3], [4,5,6], [7, 8, 9]])
 U, S, VT = np.linalg.svd(A)
-print("Matrix A:")
-print(A)
-n_comp =2 
+print("Matrix A:\n",A)
+n_comp = 2 
 A_Reconstructed = np.dot(U[:,:n_comp], np.dot(np.diag(S[:n_comp]), VT[:n_comp, :]))
-print("\nReconstructed matrix (with reduced dimension):")
-print(A_Reconstructed)
+print("\nReconstructed matrix (with reduced dimension):\n",A_Reconstructed)
