eb39577017aa5fe63c186c0a94842947d418538e
yaseen <yzac1569@gmail.com>
2025-07-15 10:37:48 +0530

    finish

---
+++ record3/svd.py
@@ -0,0 +1,13 @@
+# program 3: singular value decomposition using numpy
+
+import numpy as np
+
+
+A = np.array([[1,2,3], [4,5,6], [7, 8, 9]])
+U, S, VT = np.linalg.svd(A)
+print("Matrix A:")
+print(A)
+n_comp =2 
+A_Reconstructed = np.dot(U[:,:n_comp], np.dot(np.diag(S[:n_comp]), VT[:n_comp, :]))
+print("\nReconstructed matrix (with reduced dimension):")
+print(A_Reconstructed)
